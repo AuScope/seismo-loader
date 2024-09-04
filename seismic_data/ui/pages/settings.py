40d6ecdcf7725bfe779ff82cca6102476e8d8b7f
Ben Motevalli <mot032@CORAL-KF.nexus.csiro.au>
2024-09-04 10:23:51 +0800

    added a settings page.

---
+++ seismic_data/ui/pages/settings.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Seismo Loader",
+    page_icon="🌎",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.markdown("# Settings")
+
+
+st.write("Use this page to put app settings such as folder paths.")
+sds_path = st.text_input("SDS Path", "./data/SDS")
+db_path = st.text_input("Database Path", "./data/database.sql")
+
+
+st.write("""By the way, should we let user to change above paths at all? 
+         I guess it would be better it would be a fix location as changing the paths
+         might interrupt synchronisation.""")
