a4276e0a2f83d8611907f109d4f35d6141d501f6
Neda Taherifar <n.taherifar@gmail.com>
2024-11-18 12:35:06 +0800

    Clear the network field if the user changes the client settings #151

--- seed_vault/ui/components/base.py
+++ seed_vault/ui/components/base.py
@@ -285,11 +285,16 @@ class BaseComponent:
             with st.expander("### Filters", expanded=True):
                 client_options = list(self.settings.client_url_mapping.keys())
                 try:
+                    previous_client = self.settings.station.client
                     self.settings.station.client = st.selectbox(
                         'Choose a client:', client_options, 
                         index=client_options.index(self.settings.station.client), 
                         key="event-pg-client-station"
                     )
+                    if self.settings.station.client != previous_client:
+                        self.settings.station.network = ""
+                        st.session_state["event-pg-net-txt-station"] = self.settings.station.network
+
                 except ValueError as e:
                     st.error(f"Error: {str(e)}. Station client is set to {self.settings.station.client}, which seems does not exists. Please navigate to the settings page and use the Clients tab to add the client or fix the stored config.cfg file.")
 
